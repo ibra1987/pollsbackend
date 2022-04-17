9d86eec6208df86c20b23aad711570468b6738fe
ibra1987 <ibra1987>
2022-04-17 22:23:29 +0000

    3

--- backend/ressources/users/usersControllers/Login.js
+++ backend/ressources/users/usersControllers/Login.js
@@ -1,4 +1,5 @@
 const User = require("../../../models/User");
+const { loginValidator } = require("../usersValidator");
 
 const Login = async (req, res) => {
   const { email, password } = req.body;
